18d6ba29db414e06112e4e5ae20653cc8dd2c295
049341 <049341@UNI-LAB2-43>
2025-03-17 21:19:19 -0300

    Adicionar arquivos de projeto.

---
+++ H1/Program.cs
@@ -0,0 +1,42 @@
+﻿string[,] tabuleiro = new string[3, 3];
+string jogadorO = "O";
+string jogadorX = "X";
+string jogadorAtual = jogadorX;
+int linha, coluna;
+
+bool fimDeJogo = false;
+//fimDeJogo == false
+while (!fimDeJogo)
+{
+    Console.WriteLine("Jogador Atual: " + jogadorAtual);
+    Console.WriteLine("Digite a linha:");
+    linha = Convert.ToInt32(Console.ReadLine()) - 1;
+
+    Console.WriteLine("Digite a coluna:");
+    coluna = Convert.ToInt32(Console.ReadLine()) - 1;
+
+    if (tabuleiro[linha, coluna] == null)
+    {
+        tabuleiro[linha, coluna] = jogadorAtual;
+        if (jogadorAtual == jogadorX)
+            jogadorAtual = jogadorO;
+    }
+    else
+    {
+        Console.WriteLine("Posição está ocupada");
+    }
+    ImprimirTabuleiro();
+    Console.ReadLine();
+}
+
+void ImprimirTabuleiro()
+{
+    for (int linhaTabuleiro = 0; linhaTabuleiro < 3; linhaTabuleiro++)
+    {
+        for (int colunaTabuleiro = 0; colunaTabuleiro < 3; colunaTabuleiro++)
+        {
+            Console.Write("" + tabuleiro[linhaTabuleiro, colunaTabuleiro] + "");
+        }
+        Console.WriteLine();
+    }
+}
